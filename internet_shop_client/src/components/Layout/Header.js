39bb23f40b7871df8e98b862b9d74a8ddef660b9
Юрій <89320dota@gmail.com>
2019-01-06 14:34:53 +0200

    create header with icon. Install router

--- internet_shop_client/src/components/Layout/Header.js
+++ internet_shop_client/src/components/Layout/Header.js
@@ -3,9 +3,12 @@ import React, { Component } from 'react'
 class Header extends Component {
   render() {
     return (
-      <div>
-        <h1>Navbar</h1>
-      </div>
+      <nav className = "navbar navbar-expand-lg navbar-dark bg-dark">
+        <a href ="http://localhost:3000" className="navbar-brand">
+            <img src="https://st4.depositphotos.com/5520014/20402/v/1600/depositphotos_204026100-stock-illustration-cake-with-candle-green-neon.jpg" width="40" height="40" alt="logo"/>
+
+        </a>
+      </nav>
     )
   }
 }
